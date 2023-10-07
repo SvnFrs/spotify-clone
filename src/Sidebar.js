3dda5e02dc2810c7c3f8cd46656aca887ccf8399
“SvnFrs” <thaidvqce170410@fpt.edu.vn>
2023-10-07 16:50:54 +0700

    Add main fuctions to the website, add the gradient backgrounds.

---
+++ src/Sidebar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Sidebar.css'
+
+function Sidebar() {
+  return (
+    <div className='sidebar'>
+        <img className='sidebar-logo'
+            src='https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_CMYK_White.png' alt='yolo'/>
+    </div>
+  )
+}
+
+export default Sidebar
---
+++ src/reducer.js
@@ -0,0 +1,31 @@
+export const initialState = {
+    user: null,
+    playlist: [],
+    playing: false,
+    item: null,
+    // remove after finished developing
+    token: 'BQBMld44hTqnultncUgSfCv_kuTabFuuar56xwtweCA6jFgWJFB6z84mS3LoEpW3g50ZTg6p4n-5-7oT2rlgb4SephcajGhfe8n7C6G7ViFf5_Jl6Tka5EJP-un_qUab4-q2PtSNRgZahGhaLRmg-t_Nfp_eDEZw8P-c1HGKwGGF5XNTbtoRZNFxSV60vKvCbznRAnkBxRGaQaSnQXmJ',
+};
+
+const reducer = (state, action) => {
+    console.log(action);
+    console.log(state);
+        switch(action.type) {
+            case 'SET_TOKEN':
+                return {
+                    ...state,
+                    token: action.token,
+                }
+            
+            case 'SET_USER':
+                return {
+                    ...state,
+                    user: action.user,
+                };
+            default:
+                return state;
+        }
+
+
+} 
+export default reducer;
