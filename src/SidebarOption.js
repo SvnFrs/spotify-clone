356d3ef49acbcef3d0ace661b258d591a261fb99
“SvnFrs” <thaidvqce170410@fpt.edu.vn>
2023-11-07 22:33:56 +0700

    Do something that I am not understand

--- src/SidebarOption.js
+++ src/SidebarOption.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "./SidebarOption.css";
 function SidebarOption({ title, Icon }) {
+  const clicked = () => {
+    console.log('clicked');
+  }
   return (
     <div className="sidebarOption">
         {Icon && <Icon className="sidebarOption-icon" />}
-      {Icon ? <h4>{title}</h4> : <p>{title} </p>}
+      {Icon ? <h4>{title}</h4> : <p onClick={clicked} >{title} </p>}
     </div>
   );
 }
